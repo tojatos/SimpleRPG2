e282a3031c82fd0706f00b4bdc2ba1750072a8ae
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-01-20 23:33:09 +0100

    Shop triggers added

--- src/ui/BeforeFightController.java
+++ src/ui/BeforeFightController.java
@@ -1,16 +1,28 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import simplerpg.Character;
 import simplerpg.Game;
+import simplerpg.Item;
+import simplerpg.Shop;
 
 public class BeforeFightController {
     public ProgressBar characterHpFill;
     public Label characterNameLabel;
     public Label attackPointsLabel;
+    public Label goldLabel;
+    public Button smallHpPotButton;
+    public Button mediumHpPotButton;
+    public Button bigHpPotButton;
+    public Button woodenSwordButton;
+    public Button silverSwordButton;
+    public Button diamondSwordButton;
+    public Button leatherArmourButton;
+    public Button silverArmourButton;
+    public Button dragonSkinButton;
 
     private Game getGame() {
         return GameController.Game;
@@ -19,12 +31,41 @@ public class BeforeFightController {
     @FXML
     private void initialize() {
         Character mainCharacter = getGame().mainCharacter;
+        updateHpFill(mainCharacter);
+        characterNameLabel.setText(mainCharacter.getName());
+        attackPointsLabel.setText(Integer.toString(mainCharacter.getAttackPoints()));
+        updateGold();
+        AddBuyListeners();
+    }
+
+    private void AddBuyListeners() {
+        Character m = getGame().mainCharacter;
+        addBuyListener(smallHpPotButton,   Item.SmallHPPotion,   m);
+        addBuyListener(mediumHpPotButton,  Item.MediumHPPotion,  m);
+        addBuyListener(bigHpPotButton,     Item.BigHPPotion,     m);
+        addBuyListener(woodenSwordButton,         Item.WoodenSword,     m);
+        addBuyListener(silverSwordButton,         Item.SilverSword,     m);
+        addBuyListener(diamondSwordButton,        Item.DiamondSword,    m);
+        addBuyListener(leatherArmourButton,       Item.LeatherArmour,   m);
+        addBuyListener(silverArmourButton,        Item.SilverArmour,    m);
+        addBuyListener(dragonSkinButton,          Item.DragonSkin,      m);
+    }
+
+    private void updateGold() {
+        goldLabel.setText("Gold: " + getGame().mainCharacter.getGold());
+    }
+    private void updateHpFill(Character mainCharacter) {
         characterHpFill.setProgress(mainCharacter.getHealthPoints() / mainCharacter.getMaxHealthPoints());
         Tooltip hpTooltip = new Tooltip();
         hpTooltip.setText(mainCharacter.getHealthPoints() + "/" + mainCharacter.getMaxHealthPoints());
         characterHpFill.setTooltip(hpTooltip);
-        characterNameLabel.setText(mainCharacter.getName());
-        attackPointsLabel.setText(Integer.toString(mainCharacter.getAttackPoints()));
+    }
 
+    private void addBuyListener(Button button, Item itemToBuy, Character characterBuying)
+    {
+        button.setOnAction(e -> {
+            Shop.Buy(characterBuying, itemToBuy);
+            updateGold();
+        });
     }
 }
